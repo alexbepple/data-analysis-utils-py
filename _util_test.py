38e8b618b4f0ccc99e75529009cc68716e392593
Alex Bepple <alex@bepple.de>
2018-07-17 22:38:47 +0200

    . group tests

--- _util_test.py
+++ _util_test.py
@@ -1,6 +1,7 @@
 from ._util import unzip
 
-def test_unzip_list_of_2_tuples():
-    assert unzip([(0, 2), (1, 3)]) == ([0, 1], [2, 3])
-def test_unzip_list_of_3_tuples():
-    assert unzip([(0, 1, 2)]) == ([0], [1], [2])
+class TestUnzip(object):
+    def test_list_of_2_tuples(self):
+        assert unzip([(0, 2), (1, 3)]) == ([0, 1], [2, 3])
+    def test_list_of_3_tuples(self):
+        assert unzip([(0, 1, 2)]) == ([0], [1], [2])
