56b6e607cc12edaef6cf329698a838118b7ad391
Alex Bepple <alex@bepple.de>
2018-07-17 22:00:47 +0200

    % add test

--- bootstrap_test.py
+++ bootstrap_test.py
@@ -1,2 +1,6 @@
-def test_answer():
-    assert 1+1 == 2
+import numpy as np
+
+from . import bootstrap as bs
+
+def test_replicate_returns_numpy_array():
+    assert bs.replicate([1], np.mean, size=1) * 2 == np.array([2])
